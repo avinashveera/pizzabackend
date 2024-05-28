eb0ce2c340173bf3b15bd160bac84993d4a43618
avinashveera <avinashveera77@gmail.com>
2024-05-28 10:13:44 +0530

    first commit

--- app.js
+++ app.js
@@ -22,21 +22,16 @@ const PORT=5000
  })
 
  app.use(cors());
+
  app.options("", cors());
- app.use(function (req, res, next) 
+
+
+ app.use(function (req, res) 
  {
        res.header("Access-Control-Allow-Origin", "");
        res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
        res.header("Access-Control-Allow-Headers","Origin,X-Requested-With, X-CallbackType, Content-Type, Accept");
        res.header("Cache-Control", "no-cache");
-       if ("OPTIONS" == req.method) 
-       {
-           res.send(200);
-       }
-       else 
-      {
-        next();
-      }
  });
  
 
